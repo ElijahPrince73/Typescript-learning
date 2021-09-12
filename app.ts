b48c040f1a028b5a014b5dcb2067867e3f9178d6
Elijah <elijahprince73@gmail.com>
2021-09-11 17:36:29 -0700

    simple types

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+function add(n1: number,n2: number, showResult: boolean, phrase: string) {
+    const total = n1 + n2
+    if (showResult) {
+        console.log(phrase + total)
+    } else {
+        return total
+    }
+}
+
+const number = 2
+const number2 = 2
+const printResult = true
+const resultPhrase = 'Result is: '
+
+add(number, number2, printResult, resultPhrase);
