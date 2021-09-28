8b939a29ea797cc798b53212a6ace0b0694792ba
Elijah <elijahprince73@gmail.com>
2021-09-27 19:51:42 -0700

    Class inheritance

--- TS-Classes/src/app.ts
+++ TS-Classes/src/app.ts
@@ -1,13 +1,14 @@
 class Department {
-    name: string;
+    // private readonly id: string
+    // public name: string;
     private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n
+    constructor(private readonly id: string, public name: string) {
+        this.name = name
     }
 
     describe(this: Department) {
-        console.log('Department: ' + this.name)
+        console.log(`Department ${this.id}: ${this.name}`)
     }
 
     addEmployee(employee: string) {
@@ -19,12 +20,40 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department {
+    admins: string[];
 
-accounting.addEmployee("Max")
-accounting.addEmployee("bob");
-//accounting.employees[2] ='ss'
+    constructor(id: string, admins: string[]) {
+        super(id, "IT")
+        this.admins = admins
+    }
+}
 
-accounting.printEmployeeInfo()
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting')
+    }
+    addReports(text: string) {
+        this.reports.push(text)
+    }
+    getReports() {
+        console.log(this.reports)
+    }
+}
 
-accounting.describe()
+const accounting = new AccountingDepartment("abc123", []);
+accounting.addReports("A brand new report for taxes")
+
+const IT = new ITDepartment("d1", ["max"]);
+
+IT.addEmployee("Max");
+IT.addEmployee("bob");
+//IT.employees[2] ='ss'
+
+IT.printEmployeeInfo();
+
+IT.describe();
+
+console.log(accounting)
+
+console.log(IT)
