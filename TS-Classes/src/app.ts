bdd806a222e288771104e2bc9ad73a62a735b79a
Elijah <elijahprince73@gmail.com>
2021-09-29 16:53:04 -0700

    Getters and Setters

--- TS-Classes/src/app.ts
+++ TS-Classes/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
     // private readonly id: string
     // public name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {
         this.name = name
@@ -30,11 +30,36 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport:  string;
+
+    get mostRecentReport() {
+        if(this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found")
+    }
+    set mostRecentReport(value: string) {
+        if(!value) {
+            throw new Error("Please add value")
+        }
+        this.addReports(value)
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting')
+        this.lastReport = reports[0]
     }
+
+    addEmployee(name: string) {
+        if(name === 'Max') {
+            return
+        }
+        this.employees.push(name)
+    }
+
     addReports(text: string) {
         this.reports.push(text)
+        this.lastReport = text
     }
     getReports() {
         console.log(this.reports)
@@ -42,7 +67,9 @@ class AccountingDepartment extends Department {
 }
 
 const accounting = new AccountingDepartment("abc123", []);
+accounting.mostRecentReport = 'taxes 2021'
 accounting.addReports("A brand new report for taxes")
+accounting.addEmployee('Herbert')
 
 const IT = new ITDepartment("d1", ["max"]);
 
@@ -54,6 +81,8 @@ IT.printEmployeeInfo();
 
 IT.describe();
 
-console.log(accounting)
+console.log("Accounting ",accounting)
 
-console.log(IT)
+console.log("IT",IT)
+
+console.log("Most recent report:", accounting.mostRecentReport)
