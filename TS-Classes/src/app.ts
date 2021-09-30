0b4a63b7372dc3bcdeb07126f460add9e9aa5f6a
Elijah <elijahprince73@gmail.com>
2021-09-29 17:14:15 -0700

    Abstract classes

--- TS-Classes/src/app.ts
+++ TS-Classes/src/app.ts
@@ -1,75 +1,86 @@
-class Department {
-    // private readonly id: string
-    // public name: string;
-    protected employees: string[] = [];
+abstract class Department {
+  static fiscalYear = 2021;
+  // private readonly id: string
+  // public name: string;
+  protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
-        this.name = name
-    }
+  constructor(protected readonly id: string, public name: string) {
+    this.name = name;
+  }
 
-    describe(this: Department) {
-        console.log(`Department ${this.id}: ${this.name}`)
-    }
+  static createEmployee(name: string) {
+    return { name: name };
+  }
 
-    addEmployee(employee: string) {
-        this.employees.push(employee)
-    }
-    printEmployeeInfo() {
-        console.log(this.employees.length)
-        console.log(this.employees)
-    }
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 class ITDepartment extends Department {
-    admins: string[];
+  admins: string[];
 
-    constructor(id: string, admins: string[]) {
-        super(id, "IT")
-        this.admins = admins
-    }
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+  describe() {
+    console.log("IT department - ID " + this.id);
+  }
 }
 
 class AccountingDepartment extends Department {
-    private lastReport:  string;
+  private lastReport: string;
 
-    get mostRecentReport() {
-        if(this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error("No report found")
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
     }
-    set mostRecentReport(value: string) {
-        if(!value) {
-            throw new Error("Please add value")
-        }
-        this.addReports(value)
+    throw new Error("No report found");
+  }
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please add value");
     }
+    this.addReports(value);
+  }
 
-    constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting')
-        this.lastReport = reports[0]
-    }
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
 
-    addEmployee(name: string) {
-        if(name === 'Max') {
-            return
-        }
-        this.employees.push(name)
-    }
+  describe() {
+    console.log("Accounting department - ID " + this.id);
+  }
 
-    addReports(text: string) {
-        this.reports.push(text)
-        this.lastReport = text
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
     }
-    getReports() {
-        console.log(this.reports)
-    }
+    this.employees.push(name);
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+  getReports() {
+    console.log(this.reports);
+  }
 }
 
 const accounting = new AccountingDepartment("abc123", []);
-accounting.mostRecentReport = 'taxes 2021'
-accounting.addReports("A brand new report for taxes")
-accounting.addEmployee('Herbert')
+accounting.mostRecentReport = "taxes 2021";
+accounting.addReports("A brand new report for taxes");
+accounting.addEmployee("Herbert");
 
 const IT = new ITDepartment("d1", ["max"]);
 
@@ -81,8 +92,11 @@ IT.printEmployeeInfo();
 
 IT.describe();
 
-console.log("Accounting ",accounting)
+console.log("Accounting ", accounting);
 
-console.log("IT",IT)
+console.log("IT", IT);
 
-console.log("Most recent report:", accounting.mostRecentReport)
+console.log("Most recent report:", accounting.mostRecentReport);
+
+const employee1 = Department.createEmployee("Jerry");
+console.log("emplyoee", employee1, Department.fiscalYear);
